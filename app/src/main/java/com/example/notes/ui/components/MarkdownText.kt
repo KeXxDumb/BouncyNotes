4145f9bb366a4627c4d83a133b34c6b94403a36a
KeXxDumb <krizgameryt@gmail.com>
2026-08-15 10:54:07 -0400

    Fix: import faltante de withStyle en MarkdownText

--- app/src/main/java/com/example/notes/ui/components/MarkdownText.kt
+++ app/src/main/java/com/example/notes/ui/components/MarkdownText.kt
@@ -8,6 +8,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
