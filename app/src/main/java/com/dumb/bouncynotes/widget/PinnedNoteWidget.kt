3a7034d3ba0e5371ceba533e6e05b2cf0ee21191
KeXxDumb <krizgameryt@gmail.com>
2026-09-01 00:27:03 -0400

    Prueba de widget v2

--- app/src/main/java/com/dumb/bouncynotes/widget/PinnedNoteWidget.kt
+++ app/src/main/java/com/dumb/bouncynotes/widget/PinnedNoteWidget.kt
@@ -12,7 +12,6 @@ import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
 import androidx.glance.Image
 import androidx.glance.ImageProvider
-import androidx.glance.defaultWeight
 import androidx.glance.action.clickable
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
